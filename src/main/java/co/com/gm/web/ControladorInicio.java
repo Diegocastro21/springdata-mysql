f58cb5ed1828ba284bf743718595a8b30adcae8f
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-06-22 22:07:36 -0500

    mapeo base de datos

--- src/main/java/co/com/gm/web/ControladorInicio.java
+++ src/main/java/co/com/gm/web/ControladorInicio.java
@@ -1,26 +1,26 @@
 package co.com.gm.web;
 
+import co.com.gm.dao.PersonaDao;
 import co.com.gm.domain.Persona;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
 
-
+    @Autowired
+    private PersonaDao personaDao;
 
     @GetMapping("/")
     public String inicio(Model model){
 
+        Iterable<Persona> personas = personaDao.findAll();
         log.info("ejecutando el controlador Spring MVC");
-       // model.addAttribute("personas", personas);
+        model.addAttribute("personas", personas);
         return "index";
     }
 
